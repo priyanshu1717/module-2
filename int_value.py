059a754e5b37aa4e5446b0c1a7298501eaff092f
ddnjcjn <sorathiyapriyanshu@gmail.com>
2022-10-11 09:02:14 +0530

    Module -2

---
+++ int_value.py
@@ -0,0 +1,9 @@
+a=int(input("Enter value of A:"))
+b=int(input("Enter value of B:"))
+
+if a==b:
+    print("True")
+else:
+    print("False")
+
+
---
+++ practice.py
@@ -0,0 +1,3 @@
+n=str(input("which type you can want it ?  "))
+if 
+    n1[0,5]
